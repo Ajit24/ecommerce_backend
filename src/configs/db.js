7052cccf95ba43d57ab00a1494bc892a0a8d43b2
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-05-17 00:15:53 +0530

    make it better

--- src/configs/db.js
+++ src/configs/db.js
@@ -4,3 +4,5 @@ module.exports = () => {
   return mongoose.connect("mongodb+srv://ajit:12345@cluster0.517og.mongodb.net/ECommerce-Website?retryWrites=true&w=majority");
  
 };
+
+//https://ecommerceajit.herokuapp.com/
